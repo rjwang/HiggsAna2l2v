78dcca2924112aaee39be4df01d3a2212cc1947b
rjwang <r.jiewang@gmail.com>
2014-02-06 15:49:14 +0100

    clean the old config file

--- test/2l2vFullAnalysis_mc_cfg.py
+++
@@ -1,9 +0,0 @@
-import os,sys
-runOnMC=True
-runFull=True
-runStd=True
-cfgFile=os.path.expandvars('${CMSSW_BASE}/src/CMGTools/HiggsAna2l2v/test/2l2vPattuple_cfg.py')
-from CMGTools.HiggsAna2l2v.localPatTuples_cff import configureSourceFromCommandLine
-castorDir, outFile, inputList = configureSourceFromCommandLine()
-outFile='analysis.root'
-execfile(cfgFile)
